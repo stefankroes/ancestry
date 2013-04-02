79e5611d14fb8a560a453227a68f558ea39d1d93
Systho <systho@gmail.com>
2013-04-02 14:47:52 +0300

    Update has_ancestry.rb
    used the lambda keyword instead of stabby lambda in order to keep compatibility with older versions

--- lib/ancestry/has_ancestry.rb
+++ lib/ancestry/has_ancestry.rb
@@ -33,14 +33,14 @@ class << ActiveRecord::Base
     validate :ancestry_exclude_self
     
     # Named scopes
-    scope :roots, ->(){ where(ancestry_column => nil) }
-    scope :ancestors_of, ->(object){ where(to_node(object).ancestor_conditions) }
-    scope :children_of, ->(object){ where(to_node(object).child_conditions) }
-    scope :descendants_of, ->(object){ where(to_node(object).descendant_conditions) }
-    scope :subtree_of, ->(object){ where(to_node(object).subtree_conditions) }
-    scope :siblings_of, ->(object){ where(to_node(object).sibling_conditions) }
-    scope :ordered_by_ancestry, ->(){ reorder("(case when #{table_name}.#{ancestry_column} is null then 0 else 1 end), #{table_name}.#{ancestry_column}") }
-    scope :ordered_by_ancestry_and, ->(order){ reorder("(case when #{table_name}.#{ancestry_column} is null then 0 else 1 end), #{table_name}.#{ancestry_column}, #{order}") }
+    scope :roots, lambda { where(ancestry_column => nil) }
+    scope :ancestors_of, lambda { |object| where(to_node(object).ancestor_conditions) }
+    scope :children_of, lambda { |object| where(to_node(object).child_conditions) }
+    scope :descendants_of, lambda { |object| where(to_node(object).descendant_conditions) }
+    scope :subtree_of, lambda { |object| where(to_node(object).subtree_conditions) }
+    scope :siblings_of, lambda { |object| where(to_node(object).sibling_conditions) }
+    scope :ordered_by_ancestry, lambda { reorder("(case when #{table_name}.#{ancestry_column} is null then 0 else 1 end), #{table_name}.#{ancestry_column}") }
+    scope :ordered_by_ancestry_and, lambda { |order| reorder("(case when #{table_name}.#{ancestry_column} is null then 0 else 1 end), #{table_name}.#{ancestry_column}, #{order}") }
     
     # Update descendants with new ancestry before save
     before_save :update_descendants_with_new_ancestry
