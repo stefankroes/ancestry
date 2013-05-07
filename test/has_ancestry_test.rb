39e2268c08107481a6a882c71d070231e65e6a03
Stefan Henzen <s.f.henzen@nedforce.nl>
2013-05-07 11:00:52 +0200

    added test for #85

--- test/has_ancestry_test.rb
+++ test/has_ancestry_test.rb
@@ -775,4 +775,18 @@ class HasAncestryTreeTest < ActiveSupport::TestCase
       assert_equal 1, model.arrange.count
     end
   end
+
+   def test_sort_by_ancestry_with_block
+    AncestryTestDatabase.with_model :extra_columns => {:rank => :integer} do |model|
+      n1 = model.create!(:rank => 0)
+      n2 = model.create!(:rank => 1)
+      n3 = model.create!(:rank => 0, :parent => n1)
+      n4 = model.create!(:rank => 0, :parent => n2)
+      n5 = model.create!(:rank => 1, :parent => n1)
+      n6 = model.create!(:rank => 1, :parent => n2)
+      
+      arranged = model.sort_by_ancestry(model.all.sort_by(&:rank).reverse) {|a, b| a.rank <=> b.rank}
+      assert_equal [n1, n3, n5, n2, n4, n6].map(&:id), arranged.map(&:id)
+    end
+  end
 end
