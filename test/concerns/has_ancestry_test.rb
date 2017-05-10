stefankroes/ancestry

commit a8e95fa6bef6b192be3e8a0c78b6fdbad1f16200

Use ancestors? where appropriate

lib/ancestry/instance_methods.rb
@@ -17,7 +17,7 @@ module Ancestry
               self.ancestry_base_class.ancestry_column,
               descendant.read_attribute(descendant.class.ancestry_column).gsub(
                 /^#{self.child_ancestry}/,
-                if read_attribute(self.class.ancestry_column).blank? then id.to_s else "#{read_attribute self.class.ancestry_column }/#{id}" end
+                if ancestors? then "#{read_attribute self.class.ancestry_column }/#{id}" else id.to_s end
               )
             )
           end
@@ -82,6 +82,11 @@ module Ancestry
     end
 
     # Ancestors
+
+    def ancestors?
+      # ancestor_ids.present?
+      read_attribute(self.ancestry_base_class.ancestry_column).present?
+    end
 
     def ancestry_changed?
       changed.include?(self.ancestry_base_class.ancestry_column.to_s)
@@ -148,19 +153,19 @@ module Ancestry
     end
 
     def parent_id= parent_id
-      self.parent = if parent_id.blank? then nil else unscoped_find(parent_id) end
+      self.parent = ancestors? ? unscoped_find(parent_id) : nil
     end
 
     def parent_id
-      if ancestor_ids.empty? then nil else ancestor_ids.last end
+      ancestor_ids.last if ancestors?
     end
 
     def parent
-      if parent_id.blank? then nil else unscoped_find(parent_id) end
+      unscoped_find(parent_id) if ancestors?
     end
 
     def parent_id?
-      parent_id.present?
+      ancestors?
     end
 
     def parent_of?(node)
@@ -170,11 +175,11 @@ module Ancestry
     # Root
 
     def root_id
-      if ancestor_ids.empty? then id else ancestor_ids.first end
+      ancestors? ? ancestor_ids.first : id
     end
 
     def root
-      if root_id == id then self else unscoped_find(root_id) end
+      ancestors? ? unscoped_find(root_id) : self
     end
 
     def is_root?

commit 2d0e0fa5dc3adb894ba8eac3d24c25dc6f7dbb78

Fix check for new parent_id in #parent_id=
a8e95fa mistakenly replaced a nil check for the new parent ID in the
parent_id setter with a check for `ancestry?`. The setter would then
only set the parent if the model already had ancestors, rather than if
a parent ID had been passed.

lib/ancestry/instance_methods.rb
@@ -152,8 +152,8 @@ module Ancestry
       write_attribute(self.ancestry_base_class.ancestry_column, if parent.nil? then nil else parent.child_ancestry end)
     end
 
-    def parent_id= parent_id
-      self.parent = ancestors? ? unscoped_find(parent_id) : nil
+    def parent_id= new_parent_id
+      self.parent = new_parent_id.present? ? unscoped_find(new_parent_id) : nil
     end
 
     def parent_id

test/concerns/has_ancestry_test.rb
@@ -69,6 +69,30 @@ class HasAncestryTreeTest < ActiveSupport::TestCase
     end
   end
 
+  def test_set_parent_id
+    AncestryTestDatabase.with_model :depth => 3, :width => 3 do |model, roots|
+      root1, root2, _root3 = roots.map(&:first)
+      assert_no_difference 'root1.descendants.size' do
+        assert_difference 'root2.descendants.size', root1.subtree.size do
+          root1.parent_id = root2.id
+          root1.save!
+        end
+      end
+    end
+  end
+
+  def test_set_parent_id_with_non_default_ancestry_column
+    AncestryTestDatabase.with_model :depth => 3, :width => 3, :ancestry_column => :alternative_ancestry do |model, roots|
+      root1, root2, _root3 = roots.map(&:first)
+      assert_no_difference 'root1.descendants.size' do
+        assert_difference 'root2.descendants.size', root1.subtree.size do
+          root1.parent_id = root2.id
+          root1.save!
+        end
+      end
+    end
+  end
+
   def test_setup_test_nodes
     AncestryTestDatabase.with_model :depth => 3, :width => 3 do |model, roots|
       assert_equal Array, roots.class