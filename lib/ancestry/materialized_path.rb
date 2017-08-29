ce678823da2f741a7b47c74c041db49a791bd492
Tilo Sloboda <tilo.sloboda@gmail.com>
2017-08-29 15:06:40 -0700

    adding indirects

--- lib/ancestry/materialized_path.rb
+++ lib/ancestry/materialized_path.rb
@@ -27,6 +27,18 @@ module Ancestry
       t[ancestry_column].eq(node.child_ancestry)
     end
 
+    # indirect = anyone who is a descendant, but not a child
+    def indirect_conditions(object)
+      t = arel_table
+      node = to_node(object)
+      # rails has case sensitive matching.
+      if ActiveRecord::VERSION::MAJOR >= 5
+        t[ancestry_column].matches("#{node.child_ancestry}/%", nil, true)
+      else
+        t[ancestry_column].matches("#{node.child_ancestry}/%")
+      end
+    end
+
     def descendant_conditions(object)
       t = arel_table
       node = to_node(object)
