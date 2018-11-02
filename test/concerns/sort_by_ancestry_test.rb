d586a042d4a41c6a4f8f7e63f737a6abe3ea11a9
Keenan Brock <keenan@thebrocks.net>
2018-11-02 11:44:09 -0400

    change sorting test data
    Change the tests so they don't match a simple id ordering

--- test/concerns/sort_by_ancestry_test.rb
+++ test/concerns/sort_by_ancestry_test.rb
@@ -7,6 +7,7 @@ class SortByAncestryTest < ActiveSupport::TestCase
   #       - n3
   #       - n4
   #     - n5
+  #       - n6
   # @returns [Array<model>] list of nodes
   def build_tree(model)
     # inflate the node id to test id wrap around edge cases
@@ -17,24 +18,25 @@ class SortByAncestryTest < ActiveSupport::TestCase
     n3 = model.create!(:parent => n2)
     n4 = model.create!(:parent => n2)
     n5 = model.create!(:parent => n1)
+    n6 = model.create!(:parent => n5)
 
-    puts "create: #{n1.id}..#{n5.id}" if ENV["NODES"]
-    [n1, n2, n3, n4, n5]
+    puts "create: #{n1.id}..#{n6.id}" if ENV["NODES"]
+    [n1, n2, n3, n4, n5, n6]
   end
 
   def test_sort_by_ancestry
     AncestryTestDatabase.with_model do |model|
-      n1, n2, n3, n4, n5 = build_tree(model)
+      n1, n2, n3, n4, n5, n6 = build_tree(model)
 
       records = model.sort_by_ancestry(model.all.order(:id).reverse)
-      assert_equal [n1, n5, n2, n4, n3].map(&:id), records.map(&:id)
+      assert_equal [n1, n5, n6, n2, n4, n3].map(&:id), records.map(&:id)
     end
   end
 
   # tree is of the form
   #   - n1 (0)
-  #     - n3 (0)
-  #     - n5 (1)
+  #     - n5 (0)
+  #     - n3 (3)
   #   - n2 (1)
   #     - n4 (0)
   #     - n6 (1)
@@ -45,9 +47,9 @@ class SortByAncestryTest < ActiveSupport::TestCase
 
     n1 = model.create!(:rank => 0)
     n2 = model.create!(:rank => 1)
-    n3 = model.create!(:rank => 0, :parent => n1)
+    n3 = model.create!(:rank => 3, :parent => n1)
     n4 = model.create!(:rank => 0, :parent => n2)
-    n5 = model.create!(:rank => 1, :parent => n1)
+    n5 = model.create!(:rank => 0, :parent => n1)
     n6 = model.create!(:rank => 1, :parent => n2)
 
     puts "create: #{n1.id}..#{n6.id}" if ENV["NODES"]
@@ -60,7 +62,7 @@ class SortByAncestryTest < ActiveSupport::TestCase
       sort = -> (a, b) { a.rank <=> b.rank }
 
       records = model.sort_by_ancestry(model.all.order(:rank).reverse, &sort)
-      assert_equal [n1, n3, n5, n2, n4, n6].map(&:id), records.map(&:id)
+      assert_equal [n1, n5, n3, n2, n4, n6].map(&:id), records.map(&:id)
     end
   end
 end
