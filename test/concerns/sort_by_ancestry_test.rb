08328a21bac8e7d45909327a8a0e8f06f134323c
Keenan Brock <keenan@thebrocks.net>
2018-11-02 12:21:43 -0400

    add ordering tests
    introduce a few that fail, so the fixing methods can call them out

--- test/concerns/sort_by_ancestry_test.rb
+++ test/concerns/sort_by_ancestry_test.rb
@@ -24,12 +24,44 @@ class SortByAncestryTest < ActiveSupport::TestCase
     [n1, n2, n3, n4, n5, n6]
   end
 
-  def test_sort_by_ancestry
+  def test_sort_by_ancestry_full_tree
     AncestryTestDatabase.with_model do |model|
       n1, n2, n3, n4, n5, n6 = build_tree(model)
 
       records = model.sort_by_ancestry(model.all.order(:id).reverse)
       assert_equal [n1, n5, n6, n2, n4, n3].map(&:id), records.map(&:id)
+    end
+  end
+
+  def test_sort_by_ancestry_no_parents_siblings
+    AncestryTestDatabase.with_model do |model|
+      n1, n2, n3, n4, n5, n6 = build_tree(model)
+
+      assert_equal [n4, n3].map(&:id), model.sort_by_ancestry([n4, n3]).map(&:id)
+    end
+  end
+
+  def xtest_sort_by_ancestry_no_parents_same_level
+    AncestryTestDatabase.with_model do |model|
+      n1, n2, n3, n4, n5, n6 = build_tree(model)
+
+      assert_equal [n5, n4, n3].map(&:id), model.sort_by_ancestry([n5, n4, n3]).map(&:id)
+    end
+  end
+
+  def test_sort_by_ancestry_partial_tree
+    AncestryTestDatabase.with_model do |model|
+      n1, n2, n3, n4, n5, n6 = build_tree(model)
+
+      assert_equal [n1, n5, n2].map(&:id), model.sort_by_ancestry([n5, n2, n1]).map(&:id)
+    end
+  end
+
+  def xtest_sort_by_ancestry_missing_parent_middle_of_tree
+    AncestryTestDatabase.with_model do |model|
+      n1, n2, n3, n4, n5, n6 = build_tree(model)
+
+      assert_equal [n1, n5, n4].map(&:id), model.sort_by_ancestry([n5, n4, n1]).map(&:id)
     end
   end
 
@@ -56,13 +88,54 @@ class SortByAncestryTest < ActiveSupport::TestCase
     [n1, n2, n3, n4, n5, n6]
   end
 
-  def test_sort_by_ancestry_with_block
+  def test_sort_by_ancestry_with_block_full_tree
     AncestryTestDatabase.with_model :extra_columns => {:rank => :integer} do |model|
       n1, n2, n3, n4, n5, n6 = build_ranked_tree(model)
       sort = -> (a, b) { a.rank <=> b.rank }
 
       records = model.sort_by_ancestry(model.all.order(:rank).reverse, &sort)
       assert_equal [n1, n5, n3, n2, n4, n6].map(&:id), records.map(&:id)
+    end
+  end
+
+  def test_sort_by_ancestry_with_block_all_parents_some_children
+    AncestryTestDatabase.with_model :extra_columns => {:rank => :integer} do |model|
+      n1, n2, n3, n4, n5, n6 = build_ranked_tree(model)
+      sort = -> (a, b) { a.rank <=> b.rank }
+
+      assert_equal [n1, n5, n2].map(&:id), model.sort_by_ancestry([n2, n1, n5], &sort).map(&:id)
+    end
+  end
+
+  def test_sort_by_ancestry_with_block_no_parents_all_children
+    AncestryTestDatabase.with_model :extra_columns => {:rank => :integer} do |model|
+      n1, n2, n3, n4, n5, n6 = build_ranked_tree(model)
+      sort = -> (a, b) { a.rank <=> b.rank }
+
+      assert_equal [n5, n3, n4, n6].map(&:id), model.sort_by_ancestry([n3, n4, n5, n6], &sort).map(&:id)
+    end
+  end
+
+  # NOTE: this is non ranked. included to compare and contrast with ranked version
+  # TODO: arrange_nodes broken, dropping some parentless nodes
+  def xtest_sort_by_ancestry_paginated_missing_parents_and_children
+    AncestryTestDatabase.with_model :extra_columns => {:rank => :integer} do |model|
+      n1, n2, n3, n4, n5, n6 = build_ranked_tree(model)
+      sort = -> (a, b) { a.rank <=> b.rank }
+
+      records = model.sort_by_ancestry(model.all.order(:rank).reverse)
+      assert_equal [n3, n2, n4].map(&:id), model.sort_by_ancestry([n3, n2, n4]).map(&:id)
+    end
+  end
+
+  # TODO: arrange_nodes broken, dropping some parentless nodes
+  def xtest_sort_by_ancestry_with_block_paginated_missing_parents_and_children
+    AncestryTestDatabase.with_model :extra_columns => {:rank => :integer} do |model|
+      n1, n2, n3, n4, n5, n6 = build_ranked_tree(model)
+      sort = -> (a, b) { a.rank <=> b.rank }
+
+      records = model.sort_by_ancestry(model.all.order(:rank).reverse, &sort)
+      assert_equal [n3, n2, n4].map(&:id), model.sort_by_ancestry([n3, n2, n4], &sort).map(&:id)
     end
   end
 end
