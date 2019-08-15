fed924cd2ea557ee1e1e90640cc6327de51589a5
Denny Mueller <denny@dennymueller.de>
2019-08-15 17:48:50 +0200

    remove comment

--- lib/ancestry.rb
+++ lib/ancestry.rb
@@ -6,6 +6,5 @@ require_relative 'ancestry/has_ancestry'
 require_relative 'ancestry/materialized_path'
 
 module Ancestry
-  # ANCESTRY_PATTERN = /\A[0-9]+(\/[0-9]+)*\Z/
   ANCESTRY_PATTERN = /\A\w+(\/\w+)*\Z/
 end
