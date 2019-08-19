11411f5040d05e0c3b9cd498c968726ce705b7ee
Denny Mueller <denny@dennymueller.de>
2019-08-19 10:43:03 +0200

    use id in find instead of object

--- lib/ancestry/class_methods.rb
+++ lib/ancestry/class_methods.rb
@@ -2,7 +2,11 @@ module Ancestry
   module ClassMethods
     # Fetch tree node if necessary
     def to_node object
-      if object.is_a?(self.ancestry_base_class) then object else unscoped_where{|scope| scope.find object} end
+      if object.is_a?(self.ancestry_base_class)
+        object
+      else
+        unscoped_where { |scope| scope.find(object.try(primary_key) || object) }
+      end
     end
 
     # Scope on relative depth options
