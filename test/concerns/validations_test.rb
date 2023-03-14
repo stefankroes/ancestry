6a042c182e96a83ee2fece4ef48b343dd05e4b68
Keenan Brock <keenan@thebrocks.net>
2023-03-14 11:14:21 -0400

    test sane_ancestor_ids? method

--- test/concerns/validations_test.rb
+++ test/concerns/validations_test.rb
@@ -3,17 +3,24 @@ require_relative '../environment'
 class ValidationsTest < ActiveSupport::TestCase
   def test_ancestry_column_validation
     AncestryTestDatabase.with_model do |model|
-      node = model.create
+      node = model.create # assuming id == 1
       if AncestryTestDatabase.materialized_path2?
-        vals = ['/3/', '/10/2/', '/1/4/30/', model.ancestry_root]
+        vals = ['/3/', '/10/2/', '/9/4/30/', model.ancestry_root]
       else
-        vals = ['3', '10/2', '1/4/30', model.ancestry_root]
+        vals = ['3', '10/2', '9/4/30', model.ancestry_root]
       end
       vals.each do |value|
         node.send :write_attribute, model.ancestry_column, value
-        node.valid?; assert node.errors[model.ancestry_column].blank?
+        assert node.sane_ancestor_ids?
+        node.valid?
+        assert node.errors[model.ancestry_column].blank?
       end
+    end
+  end
 
+  def test_ancestry_column_validation_fails
+    AncestryTestDatabase.with_model do |model|
+      node = model.create
       if AncestryTestDatabase.materialized_path2?
         vals = ['/a/', '/a/b/', '/-34/']
       else
@@ -21,12 +28,14 @@ class ValidationsTest < ActiveSupport::TestCase
       end
       vals.each do |value|
         node.send :write_attribute, model.ancestry_column, value
-        node.valid?; assert !node.errors[model.ancestry_column].blank?
+        refute node.sane_ancestor_ids?
+        node.valid?
+        refute node.errors[model.ancestry_column].blank?
       end
     end
   end
 
-  def test_ancestry_column_validation_alt
+  def test_ancestry_column_validation_string_key
     AncestryTestDatabase.with_model(:id => :string, :primary_key_format => /[a-z]/) do |model|
       node = model.create(:id => 'z')
       if AncestryTestDatabase.materialized_path2?
@@ -36,9 +45,15 @@ class ValidationsTest < ActiveSupport::TestCase
       end
       vals.each do |value|
         node.send :write_attribute, model.ancestry_column, value
-        node.valid?; assert node.errors[model.ancestry_column].blank?
+        node.valid?
+        assert node.errors[model.ancestry_column].blank?
       end
+    end
+  end
 
+  def test_ancestry_column_validation_string_key_fails
+    AncestryTestDatabase.with_model(:id => :string, :primary_key_format => /[a-z]/) do |model|
+      node = model.create(:id => 'z')
       if AncestryTestDatabase.materialized_path2?
         vals = ['/1/', '/1/2/', '/a-b/c/']
       else
@@ -46,7 +61,8 @@ class ValidationsTest < ActiveSupport::TestCase
       end
       vals.each do |value|
         node.send :write_attribute, model.ancestry_column, value
-        node.valid?; assert !node.errors[model.ancestry_column].blank?
+        node.valid?
+        refute node.errors[model.ancestry_column].blank?
       end
     end
   end
@@ -56,7 +72,9 @@ class ValidationsTest < ActiveSupport::TestCase
       parent = model.create!
       child = parent.children.create!
       assert_raise ActiveRecord::RecordInvalid do
-        parent.update! :parent => child
+        parent.parent = child
+        refute parent.sane_ancestor_ids?
+        parent.save!
       end
     end
   end
