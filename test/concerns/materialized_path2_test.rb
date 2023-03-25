659952451cb86268c96dd16473ea99674b46a845
Keenan Brock <keenan@thebrocks.net>
2023-03-25 00:21:13 -0400

    split materialized path from materialized path 2 tests
    they are very copy/paste.
    These tests should only be verifying the string format
    I'm guessing other strategies will be a copy paste as well.

--- test/concerns/materialized_path2_test.rb
+++ test/concerns/materialized_path2_test.rb
@@ -1,15 +1,12 @@
 require_relative '../environment'
 
-class ValidationsTest < ActiveSupport::TestCase
+class MaterializedPath2Test < ActiveSupport::TestCase
   def test_ancestry_column_validation
+    return unless AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model do |model|
       node = model.create # assuming id == 1
-      if AncestryTestDatabase.materialized_path2?
-        vals = ['/3/', '/10/2/', '/9/4/30/', model.ancestry_root]
-      else
-        vals = ['3', '10/2', '9/4/30', model.ancestry_root]
-      end
-      vals.each do |value|
+      ['/3/', '/10/2/', '/9/4/30/', model.ancestry_root].each do |value|
         node.send :write_attribute, model.ancestry_column, value
         assert node.sane_ancestor_ids?
         node.valid?
@@ -19,14 +16,11 @@ class ValidationsTest < ActiveSupport::TestCase
   end
 
   def test_ancestry_column_validation_fails
+    return unless AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model do |model|
       node = model.create
-      if AncestryTestDatabase.materialized_path2?
-        vals = ['/a/', '/a/b/', '/-34/']
-      else
-        vals = ['a', 'a/b', '-34']
-      end
-      vals.each do |value|
+      ['/a/', '/a/b/', '/-34/'].each do |value|
         node.send :write_attribute, model.ancestry_column, value
         refute node.sane_ancestor_ids?
         node.valid?
@@ -36,14 +30,11 @@ class ValidationsTest < ActiveSupport::TestCase
   end
 
   def test_ancestry_column_validation_string_key
+    return unless AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model(:id => :string, :primary_key_format => /[a-z]/) do |model|
       node = model.create(:id => 'z')
-      if AncestryTestDatabase.materialized_path2?
-        vals = ['/a/', '/a/b/', '/a/b/c/', model.ancestry_root]
-      else
-        vals = ['a', 'a/b', 'a/b/c', model.ancestry_root]
-      end
-      vals.each do |value|
+      ['/a/', '/a/b/', '/a/b/c/', model.ancestry_root].each do |value|
         node.send :write_attribute, model.ancestry_column, value
         node.valid?
         assert node.errors[model.ancestry_column].blank?
@@ -52,14 +43,11 @@ class ValidationsTest < ActiveSupport::TestCase
   end
 
   def test_ancestry_column_validation_string_key_fails
+    return unless AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model(:id => :string, :primary_key_format => /[a-z]/) do |model|
       node = model.create(:id => 'z')
-      if AncestryTestDatabase.materialized_path2?
-        vals = ['/1/', '/1/2/', '/a-b/c/']
-      else
-        vals = ['1', '1/2', 'a-b/c']
-      end
-      vals.each do |value|
+      ['/1/', '/1/2/', '/a-b/c/'].each do |value|
         node.send :write_attribute, model.ancestry_column, value
         node.valid?
         refute node.errors[model.ancestry_column].blank?
@@ -68,6 +56,8 @@ class ValidationsTest < ActiveSupport::TestCase
   end
 
   def test_ancestry_validation_exclude_self
+    return unless AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model do |model|
       parent = model.create!
       child = parent.children.create!
--- test/concerns/materialized_path_test.rb
+++ test/concerns/materialized_path_test.rb
@@ -1,73 +1,61 @@
 require_relative '../environment'
 
-class ValidationsTest < ActiveSupport::TestCase
+class MaterializedPathTest < ActiveSupport::TestCase
   def test_ancestry_column_validation
+    return if AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model do |model|
       node = model.create # assuming id == 1
-      if AncestryTestDatabase.materialized_path2?
-        vals = ['/3/', '/10/2/', '/9/4/30/', model.ancestry_root]
-      else
-        vals = ['3', '10/2', '9/4/30', model.ancestry_root]
-      end
-      vals.each do |value|
+      ['3', '10/2', '9/4/30', model.ancestry_root].each do |value|
         node.send :write_attribute, model.ancestry_column, value
         assert node.sane_ancestor_ids?
-        node.valid?
-        assert node.errors[model.ancestry_column].blank?
+        assert node.valid?
       end
     end
   end
 
   def test_ancestry_column_validation_fails
+    return if AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model do |model|
       node = model.create
-      if AncestryTestDatabase.materialized_path2?
-        vals = ['/a/', '/a/b/', '/-34/']
-      else
-        vals = ['a', 'a/b', '-34']
-      end
-      vals.each do |value|
+      ['a', 'a/b', '-34'].each do |value|
         node.send :write_attribute, model.ancestry_column, value
         refute node.sane_ancestor_ids?
-        node.valid?
-        refute node.errors[model.ancestry_column].blank?
+        refute node.valid?
       end
     end
   end
 
   def test_ancestry_column_validation_string_key
+    return if AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model(:id => :string, :primary_key_format => /[a-z]/) do |model|
       node = model.create(:id => 'z')
-      if AncestryTestDatabase.materialized_path2?
-        vals = ['/a/', '/a/b/', '/a/b/c/', model.ancestry_root]
-      else
-        vals = ['a', 'a/b', 'a/b/c', model.ancestry_root]
-      end
-      vals.each do |value|
+      ['a', 'a/b', 'a/b/c', model.ancestry_root].each do |value|
         node.send :write_attribute, model.ancestry_column, value
-        node.valid?
-        assert node.errors[model.ancestry_column].blank?
+        assert node.sane_ancestor_ids?
+        assert node.valid?
       end
     end
   end
 
   def test_ancestry_column_validation_string_key_fails
+    return if AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model(:id => :string, :primary_key_format => /[a-z]/) do |model|
       node = model.create(:id => 'z')
-      if AncestryTestDatabase.materialized_path2?
-        vals = ['/1/', '/1/2/', '/a-b/c/']
-      else
-        vals = ['1', '1/2', 'a-b/c']
-      end
-      vals.each do |value|
+      ['1', '1/2', 'a-b/c'].each do |value|
         node.send :write_attribute, model.ancestry_column, value
-        node.valid?
-        refute node.errors[model.ancestry_column].blank?
+        refute node.sane_ancestor_ids?
+        refute node.valid?
       end
     end
   end
 
   def test_ancestry_validation_exclude_self
+    return if AncestryTestDatabase.materialized_path2?
+
     AncestryTestDatabase.with_model do |model|
       parent = model.create!
       child = parent.children.create!
